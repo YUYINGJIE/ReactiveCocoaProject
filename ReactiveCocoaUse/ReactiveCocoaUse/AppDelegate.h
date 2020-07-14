8b792365e6b8b1fd0376d99d995a772e4e90f0d5
YUYINGJIE <841167503@qq.com>
2020-07-14 16:23:36 +0800

    [deva]

--- ReactiveCocoaUse/ReactiveCocoaUse/AppDelegate.h
+++ ReactiveCocoaUse/ReactiveCocoaUse/AppDelegate.h
@@ -6,7 +6,7 @@
 //
 #import <UIKit/UIKit.h>
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-//////////////a/////
+//////////////////a/////
 @property (strong, nonatomic) UIWindow *window;/////
 
 @end
